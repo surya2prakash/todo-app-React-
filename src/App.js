006a92df4de9b3e5ea15d958634b4ae808bc5033
surya2prakash <sv251171@gmail.com>
2025-08-05 22:37:34 +0530

    day 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 
+import { Route , Routes} from 'react-router-dom';
 import './App.css';
+
+
+import HomePage from './Components/HomePage';
+import Navbar from './Components/Navbar';
+import LogIn from './Components/LogIn';
+
+
 
 function App() {
   return (
     <div className="App">
-     
+          <Navbar/>
+          
+          <div className='App_container'>
+            <Routes>
+                 <Route path='/' element={<HomePage/>}></Route>
+                 <Route path='/login' element={<LogIn/>}/>
+            </Routes>
+            </div>
+    
     </div>
   );
 }
