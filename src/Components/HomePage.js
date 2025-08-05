da50eef847de8b5f8c4e0c626c68d663a774ade3
surya2prakash <sv251171@gmail.com>
2025-08-05 22:37:03 +0530

    Home page

---
+++ src/Components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import SignUp from './SignUp'
+
+import './HomePage.css'
+
+export default function HomePage() {
+  return (
+   <div className='Home_main'>
+         <SignUp/>
+   
+   </div>
+  )
+}
